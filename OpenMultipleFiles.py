ba2fb127fcd461f144ae83cb5297ef8544cfd658
Ricardo Jorge Pais <31729218+rjpais@users.noreply.github.com>
2019-03-22 16:32:43 +0000

    Open all files in folder
    This code allows opening all existing files that have a given extension inside a selected folder.

---
+++ OpenMultipleFiles.py
@@ -0,0 +1,15 @@
+# This script opens all files in a given folder that has a given extention, F]for example txt. 
+
+from Tkinter import Tk
+from tkFileDialog import askdirectory
+
+Tk().withdraw() 
+path = askdirectory(title ="Chose folder with pre-processed samples" )
+FILES = os.listdir(path)                                              # gets the names of all files in 
+for filename in FILES:
+    extention = filename.split(".")[-1]                                # gets the extension of a file
+    if extention == "txt":
+        filepath = path + "/" + filename
+        K = open(filepath, "r")
+        print filepath
+        K.close()
